654fcf96adb3fe74a3fb640f551964c8a4895fae
mizuchan <64897375+mizsenpai@users.noreply.github.com>
2024-01-12 12:20:02 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -7,6 +7,6 @@ class helloworld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("java should be banned!");
+        System.out.println("java should be banned  forever!");
     }
 }
